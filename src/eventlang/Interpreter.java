5242143e6fa617a49c856314e897e55db7494fac
Hridesh Rajan <hridesh@gmail.com>
2017-12-30 14:17:44 -0600

    more robust frontend

---
+++ src/eventlang/Interpreter.java
@@ -0,0 +1,43 @@
+package eventlang;
+import java.io.IOException;
+
+import eventlang.Env;
+import eventlang.Value;
+import eventlang.AST.Program;
+
+/**
+ * This main class implements the Read-Eval-Print-Loop of the interpreter with
+ * the help of Reader, Evaluator, and Printer classes. 
+ * 
+ * @author hridesh
+ *
+ */
+public class Interpreter {
+	public static void main(String[] args) {
+		System.out.println("EventLang: Type a program to evaluate and press the enter key,\n" + 
+				"e.g. (define ev (event (a b)))  which creates an event\n" + 
+                "     (when ev do (+ a b)) which creates an observer expression\n" + 
+                "     (announce ev ( 2 3)) which fires event ev \n" + 
+				"then try (when ev do (* a b)) which creates another observer \n" +
+				"then try (announce ev (2 3)) which fires event ev again (note the change).\n" +
+				"Press Ctrl + C to exit.");
+		Reader reader = new Reader();
+		Evaluator eval = new Evaluator(reader);
+		Printer printer = new Printer();
+		REPL: while (true) { // Read-Eval-Print-Loop (also known as REPL)
+			Program p = null;
+			try {
+				p = reader.read();
+				if(p._e == null) continue REPL;
+				Value val = eval.valueOf(p);
+				printer.print(val);
+			} catch (Env.LookupException e) {
+				printer.print(e);
+			} catch (IOException e) {
+				System.out.println("Error reading input:" + e.getMessage());
+			} catch (NullPointerException e) {
+				System.out.println("Error:" + e.getMessage());
+			}
+		}
+	}
+}
