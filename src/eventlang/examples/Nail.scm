06e1d65360514460469b8f894d70312789b2dc6a
Hridesh Rajan <hridesh@iastate.edu>
2020-07-03 17:02:03 -0500

    add nail example

---
+++ src/eventlang/examples/Nail.scm
@@ -0,0 +1,77 @@
+(define seq7 (lambda (cmd1 cmd2 cmd3 cmd4 cmd5 cmd6 cmd7 cmd8) cmd8))
+(define seq (lambda (cmd1 cmd2) cmd2))
+
+(define nail (event (nailed)))
+(define shoe (event (attached)))
+(define horse (event (canrun)))
+(define rider (event (rode)))
+(define message (event (delivered)))
+(define battle (event (won)))
+(define kingdom (event (saved)))
+
+(seq7
+	(when nail do 
+		(if nailed 
+			(seq (print "Nail is nailed") 
+			     (announce shoe #t))
+			(seq (print "Nail is missing...")
+			     (announce shoe #f))
+		)
+	)
+	
+	(when shoe do 
+		(if attached 
+			(seq (print "Shoe is attached") 
+			     (announce horse #t))
+			(seq (print "For want of a nail the shoe was lost.")
+			     (announce horse #f))
+		)
+	)
+	
+	(when horse do 
+		(if canrun 
+			(seq (print "Horse can run") 
+			     (announce rider #t))
+			(seq (print "For want of a shoe the horse was lost.")
+			     (announce rider #f))
+		)
+	)
+	
+	(when rider do 
+		(if rode 
+			(seq (print "Rider rode.") 
+			     (announce message #t))
+			(seq (print "For want of a horse the rider was lost.")
+			     (announce message #f))
+		)
+	)
+	
+	(when message do 
+		(if delivered 
+			(seq (print "Message was delivered.") 
+			     (announce battle #t))
+			(seq (print "For want of a rider the message was lost.")
+			     (announce battle #f))
+		)
+	)
+	
+	(when battle do 
+		(if won 
+			(seq (print "Battle was won.") 
+			     (announce kingdom #t))
+			(seq (print "For want of a message the battle was lost.")
+			     (announce kingdom #f))
+		)
+	)
+	
+	(when kingdom do 
+		(if saved 
+			(seq (print "Kingdom was saved.") 
+			     (announce kingdom #t))
+			(seq (print "For want of a battle the kingdom was lost.")
+			     (print "And all for the want of a horseshoe nail."))
+		)
+	)
+	
+	(announce nail #f)
+)
